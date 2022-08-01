f1d6a1faa59402f4c65e7e0fd14eb11fe264f047
vishista-135 <vishista135@gmail.com>
2022-08-01 22:49:57 +0530

    commiting files

--- src/main/java/bike/rapido/parkingLot/ParkingLot.java
+++ src/main/java/bike/rapido/parkingLot/ParkingLot.java
@@ -1,29 +1,52 @@
 package bike.rapido.parkingLot;
 
 public class ParkingLot {
-    private int availableSlots;
+    private final int totalSlots;
+    private int filledSlots = 0;
 
-    public ParkingLot(int availableSlots)
-    {
-        this.availableSlots = availableSlots;
+    public ParkingLot(int totalSlots) {
+        this.totalSlots = totalSlots;
     }
 
-    public boolean checksSlotAvailability() {
-        if(availableSlots>0)
-            return true;
-        else
-            return false;
-    }
-    public String decrementsSlotAvailabilityByParkingVehicle() {
-        if(checksSlotAvailability())
-        {
-            availableSlots -=1;
-            return String.format("Vehicle Parked. Remaining Available Slots = %d", availableSlots);
-        }
+
+    public String parksVehicle(Vehicle vehicle) {
+        if (totalSlots > filledSlots) {
+            if (vehicle.isVehicleParked()) {
+                return "Vehicle already parked.";
+            }
+            else {
+                vehicle.park();
+                filledSlots += 1;
+                if (filledSlots == totalSlots) {
+                    return "Vehicle Parked. \n ParkingLot Is Full.";
+                }
+                return "Vehicle Parked.";
+            }
+            }
         else
             return "No Available Parking Slots";
     }
 
 
+    public String unParksVehicle(Vehicle vehicle) {
+        if (filledSlots > 0) {
+            if(vehicle.isVehicleParked()) {
+                filledSlots -= 1;
+                vehicle.unPark();
+                return "Vehicle UnParked.";
+            }
+            else {
+                return "Vehicle not yet parked.";
+            }
+        } else
+            return "All Slots Are Empty.";
+    }
+
+    public Boolean isLotFull() {
+        if (filledSlots < totalSlots)
+            return false;
+        else
+            return true;
+    }
 }
 
